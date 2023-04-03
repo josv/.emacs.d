e396445e7d3c6b9ed1a14585002714480b24f66b
John Sucaet <john.sucaet@gmail.com>
2023-04-03 23:01:13 +0200

    Add qrr

--- init.el
+++ init.el
@@ -251,6 +251,9 @@
 ;; undo
 (global-set-key "\C-z" 'undo)
 
+;; query and replace
+(defalias 'qrr 'query-replace-regexp)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
