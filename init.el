b66347c18f0ec7398eaf40540c334c07701958d7
John Sucaet <john.sucaet@gmail.com>
2023-05-31 20:19:45 +0200

    Add eglot

--- init.el
+++ init.el
@@ -9,8 +9,11 @@
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "M-h") 'help-command)
 
-;;scrollbar
+;; scrollbar
 (set-scroll-bar-mode `right)
+
+;; column number
+(column-number-mode 1)
 
 ;; disable toolbar and menubar
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
@@ -98,18 +101,27 @@
               (push '("|>" . ?\u25B7) prettify-symbols-alist))))
 
 ;; LSP client interface for Emacs
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :ensure t
-  :diminish lsp-mode
-  :hook
-  (elixir-mode . lsp-deferred)
-  :init
-  (add-to-list 'exec-path "~/elixir-ls/release/erl24")
-)
+;; (use-package lsp-mode
+;;   :commands lsp
+;;   :ensure t
+;;   :diminish lsp-mode
+;;   :hook
+;;   (elixir-mode . lsp)
+;;   :init
+;;   (add-to-list 'exec-path "~/elixir-ls/release/erl24")
+;;   :config
+;;   (lsp-register-client
+;;    (make-lsp-client :new-connection (lsp-tramp-connection "~/elixir-ls/release/erl24/language_server.sh")
+;;                     :major-modes '(elixir-mode)
+;;                     :remote? t
+;;                     :server-id 'elixir-ls-remote)
+;;    )
+;; )
 
+(use-package eglot)
 ;; yasnippet
 (use-package yasnippet)
+(yas-global-mode 1)
 
 ;; ag - silver searcher
 (use-package ag)
@@ -119,10 +131,12 @@
 
 (use-package ace-jump-mode)
 (global-set-key (kbd "C-c n") 'ace-jump-mode)
+(global-set-key (kbd "C-c b") 'ace-window)
+(global-set-key (kbd "C-;") 'other-window)
 
 ;; optionally
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+;; (use-package lsp-ui :commands lsp-ui-mode)
+;; (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 
 ; Unifies projectile and treemacs
 (use-package treemacs-projectile
@@ -259,8 +273,88 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(connection-local-criteria-alist
+   '(((:application tramp :protocol "flatpak")
+      tramp-container-connection-local-default-flatpak-profile)
+     ((:application tramp)
+      tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
+ '(connection-local-profile-alist
+   '((tramp-container-connection-local-default-flatpak-profile
+      (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin"))
+     (tramp-connection-local-darwin-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state=abcde" "-o" "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . tramp-ps-time)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-busybox-ps-profile
+      (tramp-process-attributes-ps-args "-o" "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "stat=abcde" "-o" "ppid,pgid,tty,time,nice,etime,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (user . string)
+       (group . string)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (ttname . string)
+       (time . tramp-ps-time)
+       (nice . number)
+       (etime . tramp-ps-time)
+       (args)))
+     (tramp-connection-local-bsd-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (group . string)
+       (comm . 52)
+       (state . string)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . number)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-default-shell-profile
+      (shell-file-name . "/bin/sh")
+      (shell-command-switch . "-c"))
+     (tramp-connection-local-default-system-profile
+      (path-separator . ":")
+      (null-device . "/dev/null"))))
  '(package-selected-packages
-   '(ag ace-jump-mode multi-term lsp-ui flycheck yasnippet compat treemacs-magit treemacs-projectile lsp-mode elixir-mode treemacs projectile company magit diminish use-package)))
+   '(php-mode elixir-yasnippets lsp-treemacs ag ace-jump-mode multi-term lsp-ui flycheck yasnippet compat treemacs-magit treemacs-projectile elixir-mode treemacs projectile company diminish use-package)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
