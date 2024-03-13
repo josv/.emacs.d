dcfd49f6a9dd45c97fd76cac5b8abf28ed5d8b7a
josu, John Sucaet <john.sucaet@ekinops.com>
2024-03-13 20:29:29 +0100

    winum, ag

--- init.el
+++ init.el
@@ -124,7 +124,10 @@
 (yas-global-mode 1)
 
 ;; ag - silver searcher
-(use-package ag)
+;; (use-package ag)
+(use-package ag
+  :config
+  (setq ag-arguments (append ag-arguments '("--ignore" "*~"))))
 
 ;; flycheck
 (use-package flycheck)
@@ -268,6 +271,66 @@
 ;; query and replace
 (defalias 'qrr 'query-replace-regexp)
 
+;; Frame title bar formatting to show full path of file
+(setq frame-title-format
+  '(((:eval (if (buffer-file-name)
+                (abbreviate-file-name (buffer-file-name))
+                  "%b")) " [%*]")))
+
+(setq-default
+ icon-title-format
+ (list '((buffer-file-name " %f" (dired-directory
+                                  dired-directory
+                                  (revert-buffer-function " %b"
+                                  ("%b - Dir:  " default-directory)))))))
+
+;; Hack for markdown-toc
+(setq native-comp-deferred-compilation-deny-list '("markdown-toc"))
+
+;; Winum
+(use-package winum
+  :ensure t
+  :config
+  (winum-mode))
+
+(define-key winum-keymap (kbd "C-x C-1") 'winum-select-window-1)
+(define-key winum-keymap (kbd "C-x C-2") 'winum-select-window-2)
+(define-key winum-keymap (kbd "C-x C-3") 'winum-select-window-3)
+(define-key winum-keymap (kbd "C-x C-4") 'winum-select-window-4)
+(define-key winum-keymap (kbd "C-x C-5") 'winum-select-window-5)
+(define-key winum-keymap (kbd "C-x C-6") 'winum-select-window-6)
+(define-key winum-keymap (kbd "C-x C-7") 'winum-select-window-7)
+(define-key winum-keymap (kbd "C-x C-8") 'winum-select-window-8)
+(define-key winum-keymap (kbd "C-x C-9") 'winum-select-window-9)
+
+;; Pop buffer from stack
+(defvar my-buffer-stack nil
+  "Stack of visited buffers.")
+
+(defun my-push-buffer-stack ()
+  "Push the current buffer onto the stack."
+  (unless (eq (current-buffer) (car my-buffer-stack)) ; Avoid duplicates
+    (push (current-buffer) my-buffer-stack)))
+
+(defun my-pop-buffer-stack ()
+  "Pop the buffer stack and switch to the top buffer."
+  (interactive)
+  (let ((buf (pop my-buffer-stack)))
+    (while (and buf (not (buffer-live-p buf)))
+      (setq buf (pop my-buffer-stack))) ; Skip over killed buffers
+    (if buf
+        (switch-to-buffer buf)
+      (message "Buffer stack is empty"))))
+
+(add-hook 'buffer-list-update-hook 'my-push-buffer-stack)
+
+(global-set-key (kbd "C-c p") 'my-pop-buffer-stack)
+
+(with-eval-after-load 'winum
+  (set-face-attribute 'winum-face nil
+                      :weight 'bold
+                      :foreground "Red"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -354,7 +417,7 @@
       (path-separator . ":")
       (null-device . "/dev/null"))))
  '(package-selected-packages
-   '(php-mode elixir-yasnippets lsp-treemacs ag ace-jump-mode multi-term lsp-ui flycheck yasnippet compat treemacs-magit treemacs-projectile elixir-mode treemacs projectile company diminish use-package)))
+   '(winum markdown-toc eglot php-mode elixir-yasnippets lsp-treemacs ag ace-jump-mode multi-term lsp-ui flycheck yasnippet compat treemacs-magit treemacs-projectile elixir-mode treemacs projectile company diminish use-package)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
